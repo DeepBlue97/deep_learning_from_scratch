89cf1509ef61e978ee1d48e76d109c31250ea5e0
DeepBlue97 <945048933@qq.com>
2022-04-04 20:40:04 +0800

    cuda

---
+++ utils/io_img/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+
+    std::cout << "Waiting for child thread..." << std::endl;
+    std::cout << "Child thread exited!" << std::endl;
+    return 0;
+}
