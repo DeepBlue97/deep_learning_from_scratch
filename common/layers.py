a44d7843aca2874e0f4b2191e6163231656966d1
DeepBlue97 <945048933@qq.com>
2022-04-06 00:52:35 +0800

    conv

--- common/layers.py
+++ common/layers.py
@@ -39,8 +39,8 @@ class Sigmoid:
 
 class Affine:
     def __init__(self, W, b):
-        self.W =W
-        self.b = b
+        self.W =W  # (100, 10) or (4320, 100)
+        self.b = b  # (10,)
         
         self.x = None
         self.original_x_shape = None
@@ -52,16 +52,16 @@ class Affine:
         # テンソル対応 张量对应
         self.original_x_shape = x.shape
         x = x.reshape(x.shape[0], -1)
-        self.x = x
+        self.x = x #  (100, 100) or (100, 4320)
 
-        out = np.dot(self.x, self.W) + self.b
+        out = np.dot(self.x, self.W) + self.b  # (100, 10) or (100, 100)
 
         return out
 
-    def backward(self, dout):
-        dx = np.dot(dout, self.W.T)
-        self.dW = np.dot(self.x.T, dout)
-        self.db = np.sum(dout, axis=0)
+    def backward(self, dout): # (100, 10)
+        dx = np.dot(dout, self.W.T) # (100, 100)
+        self.dW = np.dot(self.x.T, dout) # (100, 10)
+        self.db = np.sum(dout, axis=0) # (10,)
         
         dx = dx.reshape(*self.original_x_shape)  # 入力データの形状に戻す 恢复输入数据的形状（テンソル対応）张量对应
         return dx
@@ -245,10 +245,10 @@ class Convolution:
 
 class Pooling:
     def __init__(self, pool_h, pool_w, stride=2, pad=0):
-        self.pool_h = pool_h
-        self.pool_w = pool_w
-        self.stride = stride
-        self.pad = pad
+        self.pool_h = pool_h  # 2
+        self.pool_w = pool_w  # 2
+        self.stride = stride  # 2
+        self.pad = pad  # 0
         
         self.x = None
         self.arg_max = None
@@ -268,7 +268,7 @@ class Pooling:
         self.x = x
         self.arg_max = arg_max
 
-        return out
+        return out  # (100, 30, 12, 12)
 
     def backward(self, dout):
         dout = dout.transpose(0, 2, 3, 1)
