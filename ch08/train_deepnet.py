c079b008671d7afb031a0039f0592eccf8102aae
DeepBlue97 <945048933@qq.com>
2022-04-06 14:40:47 +0800

    vs

--- ch08/train_deepnet.py
+++ ch08/train_deepnet.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import sys, os
+sys.path.append(os.getcwd())  # 使得可以找到open目录
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 import matplotlib.pyplot as plt
